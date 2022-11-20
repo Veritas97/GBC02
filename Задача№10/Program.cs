fe7bf494b9f97e3c3d2e86397aa72bffdd21e5f6
Veritas97 <lykovcl@gmail.com>
2022-11-20 15:11:03 +0900

    решили задачу номер 10

--- Задача№10/Program.cs
+++ Задача№10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10:
+// Напишите программу, которая принимает на 
+// 1)вход трёхзначное число и 
+// 2)на выходе показывает вторую цифру этого числа. 
+// 3)Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = num1 / 10 % 10;
+Console.Write($"Вторая цифра {num2} от числа {num1}");
