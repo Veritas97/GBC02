e52aee4b15f6482e38c547c81444a7beda23f8d8
Veritas97 <lykovcl@gmail.com>
2022-11-20 18:04:43 +0900

    прорешали задачи с урока

---
+++ Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Resultat(int number1, int number2)
+{
+    return (number1 * number1 == number2) || (number2 * number2 == number1);
+}
+
+bool resul = Resultat(num1, num2);
+Console.Write(resul ? $"Число {num1} яляется квадратом числа {num2}" : $"Число {num1} не является квадратом числа {num2}");
