1cdb5d61d0ca11a31b3657efbc2d3e277da8231f
Veritas97 <lykovcl@gmail.com>
2022-11-20 20:32:10 +0900

    решили задачу номер 15

---
+++ Задача№15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число дня недели: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == 1) Console.WriteLine("Ваш день: Понедельник");
+if (num1 == 1) Console.WriteLine("Данный день рабочий");
+else if (num1 == 2) Console.WriteLine("Ваш день: Вторник");
+if (num1 == 2) Console.WriteLine("Данный день рабочий");
+else if (num1 == 3) Console.WriteLine("Ваш день: Среда");
+if (num1 == 3) Console.WriteLine("Данный день рабочий");
+else if (num1 == 4) Console.WriteLine("Ваш день: Четверг");
+if (num1 == 4) Console.WriteLine("Данный день рабочий");
+else if (num1 == 5) Console.WriteLine("Ваш день: Пятница");
+if (num1 == 5) Console.WriteLine("Данный день рабочий");
+else if (num1 == 6) Console.WriteLine("Ваш день: Суббота");
+if (num1 == 6) Console.WriteLine("Данный день выходной");
+else if (num1 == 7) Console.WriteLine("Ваш день: Воскресенье");
+if (num1 == 7) Console.WriteLine("Данный день выходной");
+else if (num1 >= 8) Console.Write("Такого дня не существует");
+
